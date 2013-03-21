1224ea716700af343e50d456d6875ad8e9773f34
xtbl <correodecristobal@gmail.com>
2013-03-20 23:04:44 -0600

    tst

---
+++ git-acp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+repository="https://github.com/xtbl/responsive-base-2.git";
+echo "========================="
+echo "Git: Add + Commit + Push"
+echo "========================="
+git status
+echo "Git add ."
+git add .
+read -p "commit description: " desc
+git commit -m "$desc"
+git push $repository
+echo "Done ( ^_^)"
